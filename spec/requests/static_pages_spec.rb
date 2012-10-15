5e70b803160f667e7e4aeeb328f8c38c201794d0
perkinsy <perkinsy1@gmail.com>
2012-10-15 14:14:30 +1100

    3.3.2 Passing Title Tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -32,7 +32,7 @@ describe "Static Pages" do
 
   	it "should have the h1 'About Us'" do
   		visit '/static_pages/about'
-  		page.should have_selector('h2', :text => 'About Us')
+  		page.should have_selector('h1', :text => 'About Us')
   	end
 
   	it "should have the title 'About Us'" do
